4c608316645c05406089c853ac1456bff7775967
Everton Figueirôa <evertonbpg@gmail.com>
2022-08-18 23:44:44 -0300

    Update FatorialComOuSemExibicaoDoCalculo.py

--- FatorialComOuSemExibicaoDoCalculo.py
+++ FatorialComOuSemExibicaoDoCalculo.py
@@ -1,21 +1,29 @@
-def fatorial(n, show):
-    f = 1
-    for c in range(n, 0, -1):
-        f *= c
-    print(f)
+def fatorial(n, show=False):
+    """
+    -> Calcular o fatorial de um número.
+    :param n: O valor a ser calculado.
+    :param show: Mostra ou não o calculo.
+    :return: Retorna o resultado da função.
+    """
+
     if show == True:
         f = 1
         for c in range(n, 0, -1):
             print(f'{c}', end='')
             if c == 1:
-                print(f' = {f}')
+                print(f' = ', end='')
             else:
                 print(f' x ', end='')
             f *= c
-    return f
+        return f
+    else:
+        f = 1
+        for c in range(n, 0, -1):
+            f *= c
+        return f
 
 
 question = int(input('Dígite um número: '))
-answer = fatorial(question, show=True)
-
+print(fatorial(question, show=False))
+help(fatorial)
 
