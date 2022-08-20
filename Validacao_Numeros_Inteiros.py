ace234ce63017a965267f5833db3fcd0b9a71b60
Everton Figueirôa <evertonbpg@gmail.com>
2022-08-20 20:32:59 -0300

    Update Validacao_Numeros_Inteiros.py

--- Validacao_Numeros_Inteiros.py
+++ Validacao_Numeros_Inteiros.py
@@ -1,13 +1,12 @@
 def leiaInt(a):
-    question = input(a)
     while True:
+        question = input(a)
         if question.isnumeric():
             question = int(question)
             return question
         else:
             if question.strip() == '' or question != int:
                 print('\033[4;49;91m ERRO! Digite um número inteiro válido!\033[m')
-                question = input(a)
 
 
 n = leiaInt('Dígite um número: ')
