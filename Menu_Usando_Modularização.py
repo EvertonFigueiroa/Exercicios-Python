32698d307ee10478997e7e765b4932236ee7e3d7
Everton Figueirôa <evertonbpg@gmail.com>
2022-09-04 03:04:23 -0300

    Update

--- Menu_Usando_Modularização.py
+++ Menu_Usando_Modularização.py
@@ -1,3 +1,30 @@
-from uteis import dados
+from uteis.dados import *
+from time import sleep
+from uteis.arquivo import *
 
-print(dados.menu())
+arq = 'registrodepessoas.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    sleep(1)
+    anwers = menu()
+    if anwers == 1:
+        linhas()
+        print('PESSOAS CADASTRADAS'.center(40))
+        linhas()
+        lerArquivo(arq)
+    elif anwers == 2:
+        linhas()
+        print('NOVO CADASTRO'.center(30))
+        linhas()
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrarArquivo(arq, nome, idade)
+    elif anwers == 3:
+        linhas()
+        print('Saindo do sistema... Até logo!'.center(40))
+        linhas()
+        break
+    else:
+        print(f'\033[3;49;91mDígite uma opção valida!\033[m')
--- uteis/dados/__init__.py
+++ uteis/dados/__init__.py
@@ -13,14 +13,30 @@ def sovalores(n):
             return float(question)
 
 
-def menu(a=0):
-    print('='*30)
-    print('MENU PRINCIPAL'.center(30))
-    print('=' * 30)
+def menu():
+    print('='*40)
+    print('MENU PRINCIPAL'.center(40))
+    print('=' * 40)
     print('\033[3;49;93m1\033[m - \033[3;49;94mVer pessoas cadastradas\033[m \n'
           '\033[3;49;93m2\033[m - \033[3;49;94mCadastrar nova pessoa\033[m \n'
           '\033[3;49;93m3\033[m - \033[3;49;94mSair do sistema\033[m')
-    print('=' * 30)
-    question = int(input('\033[3;49;92mSua opção: \033[m'))
-    return question
+    print('=' * 40)
+    f = leiaInt('\033[3;49;92mSua Opção: \033[m')
+    return f
+
+
+
+def linhas():
+    print(f'='*40)
+
+
+def leiaInt(a):
+    while True:
+        question = input(a)
+        if question.isnumeric():
+            question = int(question)
+            return question
+        else:
+            if question.strip() == '' or question != int:
+                print('\033[4;49;91m ERRO! Digite um número inteiro válido!\033[m')
 
